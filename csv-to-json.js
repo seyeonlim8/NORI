13f597f9892cf494fad49cf036d4b7f2d14701c4
Seyeon Lim <seyeonlim8@gmail.com>
2025-07-03 19:34:57 +0900

    added n3 vocabs

---
+++ csv-to-json.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parser');
+
+const results = [];
+
+fs.createReadStream(path.resolve(__dirname, 'n3.csv'))
+  .pipe(csv())
+  .on('data', (data) => {
+    const word = {
+      level: 'n3',
+      kanji: data['expression'],
+      furigana: data['reading'],
+      example_sentence: '추후작업',
+      meanings: [
+        {
+          language_code: 'ko',
+          word_meaning: '추후작업',
+          example_sentence_meaning: '추후작업'
+        },
+        {
+          language_code: 'en',
+          word_meaning: data['meaning'],
+          example_sentence_meaning: '추후작업'
+        }
+      ]
+    };
+    results.push(word);
+  })
+  .on('end', () => {
+    fs.writeFileSync(
+      path.resolve(__dirname, 'output.json'),
+      JSON.stringify(results, null, 2),
+      'utf-8'
+    );
+    console.log('✅ JSON 파일 생성 완료: output.json');
+  });
