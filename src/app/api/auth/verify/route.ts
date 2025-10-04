1d7091bf567cd16c8a6522195675ee6b73242c4a
Seyeon Lim <seyeonlim8@gmail.com>
2025-10-04 19:18:04 -0400

    optimize import

--- src/app/api/auth/verify/route.ts
+++ src/app/api/auth/verify/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../../lib/prisma";
-import { error } from "console";
 
 export async function POST(req: Request) {
   const { token } = await req.json();
