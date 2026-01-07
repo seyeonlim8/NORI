51d2408f674c1d13c8ed25a222c17bc423943ebb
Seyeon Lim <seyeonlim8@gmail.com>
2026-01-07 20:08:07 +0900

    fix: update parameter type in FlashcardsRoute to use Promise for better async handling

--- src/app/study/fill-in-the-blank/[level]/page.tsx
+++ src/app/study/fill-in-the-blank/[level]/page.tsx
@@ -3,7 +3,7 @@ import FillInTheBlankPage from "@/components/FillInTheBlankPage";
 export default async function FlashcardsRoute({
   params,
 }: {
-  params: { level: string };
+  params: Promise<{ level: string }>;
 }) {
   const { level } = await params;
   return <FillInTheBlankPage level={level} />;
